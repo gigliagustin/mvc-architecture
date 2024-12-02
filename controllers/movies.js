25af847672a8d71eb8b3c5a274d4ba3904170e7c
Agustin Gigli <giglimagustin@gmail.com>
2024-12-02 13:45:31 -0300

    [REFACTOR] Add dependency injection code

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,36 +1,39 @@
-import { MoviesModel } from '../models/movies.js';
 import { validateMovie, validateParamsMovie } from '../schemas/schemas.js';
 
 export class MovieController {
-  static async getAllMovies (req, res) {
-    const { genre } = req.query;
-    const movies = await MoviesModel.getAllMovies({ genre });
-    res.json(movies);
+  constructor ({ movieModel }) {
+    this.movieModel = movieModel;
   }
 
-  static async getMovieById (req, res) {
+  getAllMovies = async (req, res) => {
+    const { genre } = req.query;
+    const movies = await this.movieModel.getAllMovies({ genre });
+    res.json(movies);
+  };
+
+  getMovieById = async (req, res) => {
     const { movieId } = req.params;
-    const movie = await MoviesModel.getMovieById({ movieId });
+    const movie = await this.movieModel.getMovieById({ movieId });
 
     if (!movie) {
       res.status(404).json({ message: `Movie with id ${movieId} not found` });
     } else {
       res.json(movie);
     }
-  }
+  };
 
-  static async createMovie (req, res) {
+  createMovie = async (req, res) => {
     const result = validateMovie(req.body);
 
     if (result.error) {
       return res.status(400).json({ error: result.error.message });
     }
 
-    const newMovie = await MoviesModel.createMovie({ input: result.data });
+    const newMovie = await this.movieModel.createMovie({ input: result.data });
     res.status(201).json(newMovie);
-  }
+  };
 
-  static async updateMovie (req, res) {
+  updateMovie = async (req, res) => {
     const result = validateParamsMovie(req.body);
 
     if (result.error) {
@@ -39,19 +42,19 @@ export class MovieController {
 
     const { movieId } = req.params;
 
-    const updatedMovie = await MoviesModel.updateMovie({ movieId, input: result.data });
+    const updatedMovie = await this.movieModel.updateMovie({ movieId, input: result.data });
 
     res.json(updatedMovie);
-  }
+  };
 
-  static async deleteMovie (req, res) {
+  deleteMovie = async (req, res) => {
     const { movieId } = req.params;
-    const result = await MoviesModel.deleteMovie({ movieId });
+    const result = await this.movieModel.deleteMovie({ movieId });
 
     if (!result) {
       return res.status(404).json({ message: `Movie with id ${movieId} not found` });
     }
 
     return res.json({ message: 'Movie deleted' });
-  }
+  };
 }
--- routes/movies.js
+++ routes/movies.js
@@ -1,10 +1,15 @@
 import { Router } from 'express';
 import { MovieController } from '../controllers/movies.js';
 
-export const moviesRouter = Router();
+export const createMovieRouter = ({ movieModel }) => {
+  const moviesRouter = Router();
 
-moviesRouter.get('/', MovieController.getAllMovies);
-moviesRouter.get('/:movieId', MovieController.getMovieById);
-moviesRouter.post('/', MovieController.createMovie);
-moviesRouter.patch('/:movieId', MovieController.updateMovie);
-moviesRouter.delete('/:movieId', MovieController.deleteMovie);
+  const movieController = new MovieController({ movieModel });
+  moviesRouter.get('/', movieController.getAllMovies);
+  moviesRouter.get('/:movieId', movieController.getMovieById);
+  moviesRouter.post('/', movieController.createMovie);
+  moviesRouter.patch('/:movieId', movieController.updateMovie);
+  moviesRouter.delete('/:movieId', movieController.deleteMovie);
+
+  return moviesRouter;
+};
